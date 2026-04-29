589f00f59421002189d0d50df412d35a743a9cf3
Edu-space-IDC <camixgamer222@gmail.com>
2026-04-28 21:58:39 -0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -13,7 +13,7 @@ export function Footer() {
     {
       name: 'Facebook',
       icon: Facebook,
-      url: 'https://facebook.com/tu-iglesia',
+      url: 'https://www.facebook.com/iglesia.elshaddai.505',
       color: 'hover:text-blue-500'
     },
     {
@@ -33,7 +33,7 @@ export function Footer() {
           <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
         </svg>
       ),
-      url: 'https://tiktok.com/@tu-iglesia',
+      url: 'https://www.tiktok.com/@iglesia.el.shadda?_r=1&_t=ZS-95vqUZkocOy',
       color: 'hover:text-gray-900 dark:hover:text-white'
     },
     {
